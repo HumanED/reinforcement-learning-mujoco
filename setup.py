da1992573f3df50042016484c0c87c9881d1bb26
Ethan Cheam <ethan.cheam123@gmail.com>
2024-12-31 18:42:03 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(    
+    name="shadow_gym_mj",
+    version='0.0.1',
+    install_requires=['gymnasium', 'mujoco', 'numpy', 'matplotlib', 'scipy']
+)
+
+"""Setups the project."""
+
